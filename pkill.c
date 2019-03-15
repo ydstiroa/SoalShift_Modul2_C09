051d4e67fa6e72347eb3c72ed7cdd2e77b9dac93
ydstiroa <47847548+ydstiroa@users.noreply.github.com>
2019-03-15 17:35:21 +0700

    Soal 5b

---
+++ pkill.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <error.h>
+#include <signal.h>
+#include <unistd.h>
+#include <syslog.h>
+
+int main()
+{
+  pid_t npid;
+	FILE * gpid;
+
+	gpid = fopen ("gpid.txt", "r");
+	fscanf(gpid, "%d", &npid);
+	kill(npid,SIGKILL);
+	remove("npid.txt");
+	fclose(gpid);
+	return 0;
+}
